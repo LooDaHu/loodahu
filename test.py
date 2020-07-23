9c7ad7fac310075170e033d471ef8e1cd9707ea6
LooDaHu <651120561@qq.com>
2020-07-23 23:03:19 +0800

    inital commit

---
+++ test.py
@@ -0,0 +1,6 @@
+from selenium import webdriver
+
+driver = webdriver.Chrome(".\chromedriver.exe")
+driver.get("http://localhost:63343/loodahu/index.html?_ijt=mjgsq09fjel0q7430lobomp6rv")
+
+# driver.quit()
